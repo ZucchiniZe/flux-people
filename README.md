e0396475ce77b1bee06cf1adecd7560ffef7cc44
ZucchiniZe <me@alexb.ninja>
2015-03-12 14:36:37 -0700

    prepare for gh-pages

--- README.md
+++
@@ -1,4 +0,0 @@
-Flux People
-===========
-
-A quick flux example that I a creating for my CS class so I can learn how to use flux
--- scripts/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import App from './components/App';
-
-// Actually render the app on the page
-React.render(<App/>, document.body);
